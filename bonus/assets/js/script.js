9d34a09854e566311338fa160705410d29fee59d
rolindaz <rolinda.zamboni@gmail.com>
2025-06-16 17:23:07 +0200

    Clear email list before fetching new emails on button click

--- bonus/assets/js/script.js
+++ bonus/assets/js/script.js
@@ -2,16 +2,12 @@ const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 const ulEl = document.getElementById('mail-list');
 const buttonEl = document.querySelector('button');
 
-console.log(ulEl.innerHTML);
-
-
 buttonEl.addEventListener('click', ()=>{
     for (let i = 10; i > 0; i--) {
         ulEl.innerHTML = null;
         fetch(endpoint)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
             let emailMarkup = `
                 <li>
                     ${data.response}
