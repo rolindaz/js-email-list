8fddb81b6eb982f5ba4115d1bfc40cc94ee2a947
rolindaz <rolinda.zamboni@gmail.com>
2025-06-16 17:20:05 +0200

    Add button to generate random email addresses and update HTML structure

--- bonus/assets/js/script.js
+++ bonus/assets/js/script.js
@@ -1,16 +1,25 @@
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 const ulEl = document.getElementById('mail-list');
+const buttonEl = document.querySelector('button');
 
-for (let i = 10; i > 0; i--) {
-    fetch(endpoint)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        let emailMarkup = `
-            <li>
-                ${data.response}
-            </li>
-        `;
-        ulEl.insertAdjacentHTML('beforeend', emailMarkup);
-    });
-};
+console.log(ulEl.innerHTML);
+
+
+buttonEl.addEventListener('click', ()=>{
+    for (let i = 10; i > 0; i--) {
+        ulEl.innerHTML = null;
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let emailMarkup = `
+                <li>
+                    ${data.response}
+                </li>
+            `;
+            ulEl.insertAdjacentHTML('beforeend', emailMarkup);
+        });
+    };
+});
+
+
